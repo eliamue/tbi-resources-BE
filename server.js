11db90740c93b5d8126cf2b25b79ce0122e3eb94
Elise Muellerleile <eliamue@live.com>
2022-01-09 16:53:01 -0600

    require syntax updated

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require('./lib/app');
-const pool = require('./lib/utils/pool');
+import app from './lib/app.js';
+import pool from './lib/utils/pool.js';
 
 const PORT = process.env.PORT || 7890;
 
--- setup-db.js
+++ setup-db.js
@@ -1,5 +1,5 @@
-const pool = require('./lib/utils/pool');
-const setup = require('./data/setup');
+import pool from './lib/utils/pool.js';
+import setup from './data/setup.js';
 
 setup(pool)
   .catch((err) => console.error(err))
