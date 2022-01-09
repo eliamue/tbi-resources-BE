5c756726b332ecd59c0fa12d1f5b78f18b0650ff
Elise Muellerleile <eliamue@live.com>
2022-01-09 16:18:29 -0600

    Get all resources test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,4 +28,41 @@ describe('tbi-resources-BE routes', () => {
       ...resource,
     });
   });
+
+  it('gets all resources', async () => {
+    const resource1 = await Resources.createResource({
+      title: 'Dark Reader',
+      category: 'Accessibility',
+      about:
+        'Dark Reader is an open-source browser extension that allows you to switch any website into dark mode, and gives you the ability to fully configure the theme to your preferences.',
+      link: 'https://darkreader.org/',
+      logo: 'https://repository-images.githubusercontent.com/26682105/4d22e080-070e-11eb-8930-c69a17260e45',
+    });
+
+    const resource2 = await Resources.createResource({
+      title: 'f.lux',
+      category: 'Accessibility',
+      about:
+        'F.lux is eye-care software that adapts the color of your computer display based on the time of day, for reducing eye-strain. Color filtering can also be applied always, regardless of time of day.',
+      link: 'https://justgetflux.com/',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/F.lux_logo.svg/1200px-F.lux_logo.svg.png',
+    });
+
+    const resource3 = await Resources.createResource({
+      title: 'Brain Injury Alliance',
+      category: 'Advocacy',
+      about:
+        'The mission of the Brain Injury Alliance is to raise awareness and enhance the quality of life for all people affected by brain injury. State chapter of BIA provide resource facilitation, education, outreach, events, case management, and many other resources for living with TBI.',
+      link: 'https://usbia.org/',
+      logo: 'https://usbia.org/wp-content/uploads/2015/10/USBIA-Logo.png',
+    });
+
+    const res = await request(app).get('/api/v1/resources');
+
+    expect(res.body).toEqual([
+      resource1,
+      resource2,
+      resource3
+    ]);
+  });
 });
