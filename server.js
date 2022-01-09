b2f2df6c18434aa3bebb6fee635e44a9be93475d
Elise Muellerleile <eliamue@live.com>
2022-01-09 15:36:24 -0600

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./lib/app');
+const pool = require('./lib/utils/pool');
+
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Started on ${PORT}`);
+});
+
+process.on('exit', () => {
+  console.log('Goodbye!');
+  pool.end();
+});
