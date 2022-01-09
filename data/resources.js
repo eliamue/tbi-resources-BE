283022b94119b1dec7541238a35646cefa3fe8e4
Elise Muellerleile <eliamue@live.com>
2022-01-09 16:37:17 -0600

    Resources hard coded data array added

---
+++ data/resources.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    title: 'Dark Reader',
+    category: 'Accessibility',
+    about:
+      'Dark Reader is an open-source browser extension that allows you to switch any website into dark mode, and gives you the ability to fully configure the theme to your preferences.',
+    link: 'https://darkreader.org/',
+    logo: 'https://repository-images.githubusercontent.com/26682105/4d22e080-070e-11eb-8930-c69a17260e45',
+  },
+  {
+    title: 'f.lux',
+    category: 'Accessibility',
+    about:
+      'F.lux is eye-care software that adapts the color of your computer display based on the time of day, for reducing eye-strain. Color filtering can also be applied always, regardless of time of day.',
+    link: 'https://justgetflux.com/',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/F.lux_logo.svg/1200px-F.lux_logo.svg.png',
+  },
+  {
+    title: 'Brain Injury Alliance',
+    category: 'Advocacy',
+    about:
+      'The mission of the Brain Injury Alliance is to raise awareness and enhance the quality of life for all people affected by brain injury. State chapter of BIA provide resource facilitation, education, outreach, events, case management, and many other resources for living with TBI.',
+    link: 'https://usbia.org/',
+    logo: 'https://usbia.org/wp-content/uploads/2015/10/USBIA-Logo.png',
+  },
+];
